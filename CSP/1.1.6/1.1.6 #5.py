ac30157a5dbf00993f3681960716d28637b08c75
Gabe Rinck <gaberinck05@gmail.com>
2021-09-09 13:04:44 -0700

    spider change

--- CSP/1.1.6/1.1.6 #5.py
+++ CSP/1.1.6/1.1.6 #5.py
@@ -1,20 +1,39 @@
-#   a116_buggy_image.py
+
 import turtle as trtl
-# instead of a descriptive name of the turtle such as painter,
-# a less useful variable name x is used
+
+# paint body
 painter = trtl.Turtle()
 painter.pensize(40)
-painter.circle(20)
-"""w = 6
-y = 70
-z = 380 / w
+painter.shape("circle")
+painter.shapesize(3)
+painter.stamp()
+
+# configure legs
 painter.pensize(5)
-n = 0
-while (n < w):
+legs = 8
+leg_length = 70
+spacing = 200 / legs
+right_legs = 0
+left_legs = 0
+legloop1 = 0
+legloop2 = 0
+loop = 0
+
+# draw legs
+print("spaceing=", spacing)
+while (legloop1 < 4):
   painter.goto(0,0)
-  painter.setheading(z*n)
-  painter.forward(y)
-  n = n + 1
-painter.hideturtle()"""
+  painter.setheading(spacing*right_legs+45)
+  painter.forward(leg_length)
+  right_legs = right_legs + 1
+  legloop1 = legloop1 + 1
+while (legloop2 < 4):
+  painter.goto(0,0)
+  painter.setheading(spacing*left_legs-45)
+  painter.forward(leg_length)
+  left_legs = left_legs - 1
+  legloop2 = legloop2 + 1
+
+painter.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
